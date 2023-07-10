9d705667bf0e199cfd2923292099f7b4d5ff4a8b
owen-han-gpex <owen@gpex.io>
2023-07-10 15:06:18 +0900

    init project

---
+++ pages/api/hello.js
@@ -0,0 +1,23 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+// import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss";
+
+export default function hello(req, res) {
+
+    let body =
+        {
+            name: 'John Doe',
+
+        };
+
+    body.node_env = process.env.NODE_ENV
+    try {
+        body.envName = process.env.ENV_NAME
+        body.payloadConfigPath = process.env.PAYLOAD_CONFIG_PATH
+        body.findme = process.env.FIND_ME
+        body.test = process.env.TEST
+    } catch (e) {
+        console.log(e)
+    }
+    res.status(200).json(body)
+}
